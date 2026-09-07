dc81cdd96aba7302a7bda8d47179545a6654afd0
MarkovSergei <165809659+MarkovSergei@users.noreply.github.com>
2026-09-08 00:23:03 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MAX_BOT_TOKEN = os.getenv("MAX_BOT_TOKEN", "")
-ADMIN_TOKEN = os.getenv("ADMIN_TOKEN", "")
-PORT = int(os.getenv("PORT", 3000))
